3ab579091d8bef0450b3974da9c045e8b6de0167
Roger Coll <rogercoll@protonmail.com>
2023-07-21 16:24:45 +0200

    feat: add simple example for http client

---
+++ opamp-client/examples/simple_http_client.rs
@@ -0,0 +1,89 @@
+use std::{thread::sleep, time::Duration};
+
+use http::HeaderMap;
+use log::info;
+use opamp_client::{
+    httpclient::HttpClient,
+    opamp::proto::{
+        AgentCapabilities, AgentHealth, OpAmpConnectionSettings, ServerErrorResponse,
+        ServerToAgentCommand,
+    },
+    operation::{
+        agent::Agent,
+        callbacks::{Callbacks, MessageData},
+    },
+    OpAMPClient, OpAMPClientHandle,
+};
+
+use thiserror::Error;
+#[derive(Error, Debug)]
+pub enum AgentError {
+    #[error("`{0}`")]
+    Testing(String),
+}
+struct AgentMock;
+
+impl Agent for AgentMock {
+    type Error = AgentError;
+    fn get_effective_config(
+        &self,
+    ) -> Result<opamp_client::opamp::proto::EffectiveConfig, Self::Error> {
+        Ok(opamp_client::opamp::proto::EffectiveConfig { config_map: None })
+    }
+}
+
+struct CallbacksMock;
+
+#[derive(Error, Debug)]
+pub(crate) enum CallbacksMockError {}
+
+impl Callbacks for CallbacksMock {
+    type Error = CallbacksMockError;
+    fn on_error(&self, _err: ServerErrorResponse) {}
+    fn on_connect(&self) {
+        info!("On connect callback called!")
+    }
+    fn on_message(&self, _msg: MessageData) {}
+    fn on_command(&self, _command: &ServerToAgentCommand) -> Result<(), Self::Error> {
+        Ok(())
+    }
+    fn on_connect_failed(&self, _err: Self::Error) {}
+    fn on_opamp_connection_settings(
+        &self,
+        _settings: &OpAmpConnectionSettings,
+    ) -> Result<(), Self::Error> {
+        Ok(())
+    }
+    fn on_opamp_connection_settings_accepted(&self, _settings: &OpAmpConnectionSettings) {}
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    let mut headers = HeaderMap::new();
+    headers.insert("super-key", "suuuuuuuperstrongkey".parse().unwrap());
+
+    let client = HttpClient::new(
+        AgentMock {},
+        "https://127.0.0.1/v1/opamp",
+        Some(headers),
+        AgentCapabilities::ReportsStatus,
+        CallbacksMock {},
+    )
+    .unwrap();
+
+    let mut client = client.start().await.unwrap();
+
+    client
+        .set_health(&AgentHealth {
+            healthy: true,
+            start_time_unix_nano: 1689942447,
+            last_error: "".to_string(),
+        })
+        .await
+        .unwrap();
+
+    sleep(Duration::from_secs(30));
+
+    client.stop().await.unwrap()
+}
